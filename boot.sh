e59ab5cd6a3d3cd1d27ae78e6601fcafc722eede
Maged Rawash <magedrawash@live.com>
2021-09-02 00:55:31 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,9 +1,9 @@
-GNU nano 3.2                                    boot.sh
-
 echo "the bash test number "
+cd ..
 sudo rm -r  frappe-docker-setup
 sudo git clone https://github.com/MagedMRawash/frappe-docker-setup.git
 cd frappe-docker-setup
 echo $(pwd)
+sudo apt install supervisor
 sudo cp config/ens.conf /etc/supervisor/conf.d/
 sudo service supervisor start
