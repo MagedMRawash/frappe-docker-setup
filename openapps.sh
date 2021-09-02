e96ba6261ce0c283945bd09f47ef5015255912d2
Maged Rawash <magedrawash@live.com>
2021-09-02 22:50:15 +0200

    Update openapps.sh

--- openapps.sh
+++ openapps.sh
@@ -1,19 +1,10 @@
-
 sudo cp config/ens.conf /etc/supervisor/conf.d/
-
-
 sudo service supervisor start
-echo \n  "openapps" \n
+
+echo "openapps"
 
 screen  -d -m -S vs bash -c 'code-server'
 
-
+chmod 0760 /etc/supervisor/supervisord.conf
 chmod +x './apps/frappe.sh'
-sudo -H -u frappe  -c './apps/frappe.sh'
-
-
-
-sudo ln -s `pwd`/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart-all
+sudo -i -H  -u frappe <<< $(pwd)/apps/frappe.sh
