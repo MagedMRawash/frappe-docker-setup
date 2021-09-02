130689a3b20c92f53da11453def7d11c2796fd31
Maged Rawash <magedrawash@live.com>
2021-09-02 05:55:15 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,9 +1,9 @@
 echo "the bash test number "
 cd ..
-sudo rm -r  frappe-docker-setup
+sudo rm -rf  frappe-docker-setup
 sudo git clone https://github.com/MagedMRawash/frappe-docker-setup.git
 cd frappe-docker-setup
 echo $(pwd)
 
-sudo cp config/ens.conf /etc/supervisor/conf.d/
+# sudo cp config/ens.conf /etc/supervisor/conf.d/
 sudo service supervisor start
