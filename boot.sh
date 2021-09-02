ea694f7a527cd2092dd74adbb29f553b879d0f9c
Maged Rawash <magedrawash@live.com>
2021-09-02 05:34:58 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -4,6 +4,6 @@ sudo rm -r  frappe-docker-setup
 sudo git clone https://github.com/MagedMRawash/frappe-docker-setup.git
 cd frappe-docker-setup
 echo $(pwd)
-sudo apt install supervisor
+
 sudo cp config/ens.conf /etc/supervisor/conf.d/
-sudo service supervisor start
+#sudo service supervisor start
