9192f7de93565f9f93620de0ad4af69e6c203d11
Maged Rawash <magedrawash@live.com>
2021-09-02 07:26:04 +0200

    Update openapps.sh

--- openapps.sh
+++ openapps.sh
@@ -1,3 +1,11 @@
-# sudo cp config/ens.conf /etc/supervisor/conf.d/
-# sudo service supervisor start
+sudo cp config/ens.conf /etc/supervisor/conf.d/
+cd /home/frappe/frappe-bench 
+bench start 
+
+bench config dns_multitenant off
+bench setup supervisor
+sudo ln -s `pwd`/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
+
+sudo service supervisor start
+
 echo "openapps"
