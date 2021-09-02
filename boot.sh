6011d35c45cf48b475b5b4a1c310c5fb1943c80f
Maged Rawash <magedrawash@live.com>
2021-09-02 08:21:17 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 echo "ensoulify Time " $(date)
+
+#/// nano ~/.config/code-server/config.yaml
+cat << EOF >  ~/.config/code-server/config.yaml
+bind-addr: 0.0.0.0:8005
+auth: password
+password: awsec2
+cert: false
+user-data-dir : .
+EOF
+
+
 # cd ..
 # sudo rm -rf  frappe-docker-setup
 sudo git pull
 # git clone https://github.com/MagedMRawash/frappe-docker-setup.git
 # cd frappe-docker-setup
 echo $(pwd)
+
 
 bash ./openapps.sh
 
