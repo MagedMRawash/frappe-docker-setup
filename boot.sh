4806cb21686b5bc8dede0462c43f2bb5350adedb
Maged Rawash <magedrawash@live.com>
2021-09-02 06:28:36 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@
 echo "ensoulify Time " $(date)
 # cd ..
 # sudo rm -rf  frappe-docker-setup
-# sudo git pull
+sudo git pull
 # git clone https://github.com/MagedMRawash/frappe-docker-setup.git
 # cd frappe-docker-setup
 echo $(pwd)
