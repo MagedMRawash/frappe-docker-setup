8ed0026f6b3d3093d8282650b47c1f635d7f4ba7
Maged Rawash <magedrawash@live.com>
2021-09-02 05:35:45 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
+GNU nano 3.2                                    init.sh
 
 #////// Init Docker Config
-apt install code-server
+sudo apt-get update  -y && sudo apt-get upgrade -y
+curl -fsSL https://code-server.dev/install.sh | sh
+
 
 #/// nano ~/.config/code-server/config.yaml
 cat << EOF >  ~/.config/code-server/config.yaml
@@ -10,3 +13,8 @@ password: awsec2
 cert: false
 user-data-dir : .
 EOF
+
+
+sudo apt-get install python-setuptools -y
+sudo easy_install supervisor
+sudo apt install supervisor -y
