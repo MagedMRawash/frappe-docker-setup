3d1c940e9c8a3739688951af4b0a1729702342f8
Maged Rawash <magedrawash@live.com>
2021-09-02 07:55:15 +0200

    Update openapps.sh

--- openapps.sh
+++ openapps.sh
@@ -2,15 +2,12 @@ sudo cp config/ens.conf /etc/supervisor/conf.d/
 
 sudo apt-get install  dialog apt-utils -y  
 sudo apt-get install screen -y  
-
 sudo service supervisor start
-
 echo "openapps"
 
 
 su - frappe 
 cd /home/frappe/frappe-bench 
-screen  -d -m -S bench bash -c 'bench start'
 
 
 bench config dns_multitenant off
@@ -19,4 +16,6 @@ sudo ln -s `pwd`/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart-all
+
+screen  -d -m -S bench bash -c 'bench start'
 
