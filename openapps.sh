b059db77fb552a5611560fd9359313e301ab9d7a
Mostafa-Rawash <mostafa@rawash.com>
2021-09-02 10:56:29 +0200

    Update openapps.sh

--- openapps.sh
+++ openapps.sh
@@ -7,11 +7,11 @@ screen  -d -m -S vs bash -c 'code-server'
 
 #sudo -i -u frappe bash << EOF
 
-cd /home/frappe/frappe-bench 
-sudo -u frappe  bench config dns_multitenant off 
-sudo -u frappe  bench setup supervisor 
-sudo -u frappe  screen  -d -m -S bench bash -c 'bench start'
-echo $(whoami)
+#cd /home/frappe/frappe-bench 
+#sudo -u frappe  bench config dns_multitenant off 
+#sudo -u frappe  bench setup supervisor 
+#sudo -u frappe  screen  -d -m -S bench bash -c 'bench start'
+#echo $(whoami)
 sudo ln -s `pwd`/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
 supervisorctl reread 
 supervisorctl update 
