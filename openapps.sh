e0dac165d354a956c9c9c0c66fb5981428c246c4
Maged Rawash <magedrawash@live.com>
2021-09-02 08:14:14 +0200

    Update openapps.sh

--- openapps.sh
+++ openapps.sh
@@ -4,20 +4,15 @@ sudo apt-get install  dialog apt-utils -y
 sudo apt-get install screen -y  
 sudo service supervisor start
 echo \n  "openapps" \n 
-
 screen  -d -m -S vs bash -c 'code-server'
-
 sudo -i -u frappe bash << EOF
 cd /home/frappe/frappe-bench 
-
 bench config dns_multitenant off 
 bench setup supervisor 
 screen  -d -m -S bench bash -c 'bench start'
 echo $(whoami)
 EOF
-
 cd /home/frappe/frappe-bench 
-
 sudo ln -s `pwd`/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
 supervisorctl reread 
 supervisorctl update 
