fcf785525c7807c8b1900727ccddc012d26f7033
Maged Rawash <magedrawash@live.com>
2021-09-02 22:51:37 +0200

    Update frappe.sh

--- apps/frappe.sh
+++ apps/frappe.sh
@@ -2,11 +2,20 @@ cd /home/frappe/frappe-bench
 
 echo $(whoami) "$USER"
 
-echo 'starting config dns_multitenant off'
+echo 'starting config dns_multitenant off' $(pwd)
 bench config dns_multitenant off
-echo 'Ending config dns_multitenant off'                                             $
+echo 'Ending config dns_multitenant off'                       
 
 echo 'starting setup supervisor'
-bench setup supervisor
-echo 'Ending config dns_multitenant off'
-#screen  -d -m -S bench bash -c 'bench start'
+#chmod 0760 /etc/supervisor/supervisord.conf
+#chown frappe:frappe /etc/supervisor/supervisord.conf
+bench setup supervisor  --yes
+echo 'Ending config supervisor '
+sudo ln -sf `pwd`/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
+
+supervisorctl reread
+supervisorctl update
+
+sudo service supervisor stop
+sudo service supervisor start
+screen  -d -m -S bench bash -c 'bench start'
