bbc4c8e8ee8296922d202633af1f2be69ace80f8
Maged Rawash <magedrawash@live.com>
2021-09-02 23:13:24 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ curl -fsSL https://code-server.dev/install.sh | sh
 sudo apt-get install  dialog apt-utils -y  
 sudo apt-get install screen -y  
 
-sudo apt-get install screen 
+
 
 
 sudo apt-get update  -y && sudo apt-get upgrade -y
@@ -15,3 +15,6 @@ hash -r
 sudo apt-get install python-setuptools -y
 sudo easy_install supervisor
 sudo apt install supervisor -y
+
+
+screen  -d -m -S vs bash -c 'code-server'
