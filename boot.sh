80f0668e15d9e3f5fe99179a307d88afff74d4e8
Maged Rawash <magedrawash@live.com>
2021-09-02 05:59:28 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,5 +5,4 @@ sudo git clone https://github.com/MagedMRawash/frappe-docker-setup.git
 cd frappe-docker-setup
 echo $(pwd)
 
-# sudo cp config/ens.conf /etc/supervisor/conf.d/
-sudo service supervisor start
+bash ./openapps.sh
