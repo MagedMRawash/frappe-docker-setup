d219bd3d17be7d25918d43ed02957c5909860271
Maged Rawash <magedrawash@live.com>
2021-09-02 07:46:33 +0200

    Update openapps.sh

--- openapps.sh
+++ openapps.sh
@@ -1,6 +1,8 @@
 sudo cp config/ens.conf /etc/supervisor/conf.d/
 
-sudo apt-get install  dialog apt-utils screen -y  
+sudo apt-get install  dialog apt-utils -y  
+sudo apt-get install screen -y  
+
 su - frappe 
 cd /home/frappe/frappe-bench 
 screen  -d -m -S bench bash -c 'bench start'
