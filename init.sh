37d44b281201067a7da656c5519c67c79f917834
Maged Rawash <magedrawash@live.com>
2021-09-02 08:20:17 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,21 +2,14 @@
 #////// Init Docker Config
 curl -fsSL https://code-server.dev/install.sh | sh
 
+sudo apt-get install  dialog apt-utils -y  
+sudo apt-get install screen -y  
 
 sudo apt-get install screen 
 
 
 sudo apt-get update  -y && sudo apt-get upgrade -y
 hash -r
-
-#/// nano ~/.config/code-server/config.yaml
-cat << EOF >  ~/.config/code-server/config.yaml
-bind-addr: 0.0.0.0:8005
-auth: password
-password: awsec2
-cert: false
-user-data-dir : .
-EOF
 
 
 sudo apt-get install python-setuptools -y
