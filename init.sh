9f89d145456ce92f4eefe0214ad4363a7ea6c822
Maged Rawash <magedrawash@live.com>
2021-09-03 03:47:51 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,4 +16,13 @@ sudo apt-get install python-setuptools -y
 sudo easy_install supervisor
 sudo apt install supervisor -y
 
+#/// nano ~/.config/code-server/config.yaml
+touch ~/.config/code-server/config.yaml
+sudo cat << EOF >  ~/.config/code-server/config.yaml
+bind-addr: 0.0.0.0:8005
+auth: password
+password: awsec2
+cert: false
+user-data-dir : /home/frappe/
+EOF
 
