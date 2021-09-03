b853f4265949636d2722f267a8d590b0f2110bac
Maged Rawash <magedrawash@live.com>
2021-09-03 04:18:26 +0200

    Update vs.sh

--- apps/vs.sh
+++ apps/vs.sh
@@ -1,9 +1,9 @@
 
 
 #/// nano ~/.config/code-server/config.yaml
-sudo mkdir ~/.config/code-server
-touch ~/.config/code-server/config.yaml
-sudo cat << EOF >  ~/.config/code-server/config.yaml
+sudo mkdir -f ~/.config/code-server
+sudo touch ~/.config/code-server/config.yaml
+sudo cat >  ~/.config/code-server/config.yaml  << EOF 
 bind-addr: 0.0.0.0:8005
 auth: password
 password: awsec2
